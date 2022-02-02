7e10cec9f6f4e3afacb6f57d3146b323771aac4e
Elena <elle.alexxandra@gmail.com>
2022-02-02 18:10:58 +0200

    phase 1: created the components and the app's structure

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import {Recipe} from "../recipe.model";
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe('TestRecipe', 'This is a test', 'https://images.immediate.co.uk/production/volatile/sites/30/2013/05/Puttanesca-fd5810c.jpg?quality=90&webp=true&resize=300,272'),
+    new Recipe('TestRecipe', 'This is a test', 'https://images.immediate.co.uk/production/volatile/sites/30/2013/05/Puttanesca-fd5810c.jpg?quality=90&webp=true&resize=300,272')
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
